1b0e5538eef5c6b316d16e014346354fee887c7b
scorpion77777 <vvmartirosyan@gmail.com>
2021-11-14 16:45:10 -0800

    first commit

---
+++ App/index.js
@@ -0,0 +1,64 @@
+//Packages and modules needed for this application
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of the project?",
+      name: "title",
+    },
+
+    { type: "input", message: "Describe your project:", name: "description" },
+
+    {
+      type: "input",
+      message: "How do you install your project?",
+      name: "install",
+    },
+
+    { type: "input", message: "How is your project used?", name: "usage" },
+
+    {
+      type: "input",
+      message: "What tests run with your project?",
+      name: "tests",
+    },
+
+    {
+      type: "input",
+      message: "How can people contribute to your project?",
+      name: "contribute",
+    },
+
+    {
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "github",
+    },
+
+    { type: "input", message: "What is your email address?", name: "email" },
+
+    {
+      name: "license",
+      type: "list",
+      message: "Choose your license:",
+      choices: ["MIT", "Apache", "GPL", "BSD3"],
+    },
+  ]);
+};
+
+// Function to intialize the app
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync("README.md", generateMarkdown(answers)))
+    .then(() => console.log("Successfully wrote to README.md"))
+    .catch((err) => console.error(err));
+};
+
+// Function call to initialize app
+init();
---
+++ App/utils/generateMarkdown.js
@@ -0,0 +1,78 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+//function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+//function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+//function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+//function generateMarkdown(data) {
+// return `# ${data.title}
+
+//`;
+//}
+
+//module.exports = generateMarkdown;
+
+//This function generates the markdown document
+const generateMarkdown = (data) => {
+  //This switch case takes the input of the license prompts and assigns a url to the proper license
+  let licenseLink = "";
+
+  switch (data.license) {
+    case "MIT":
+      licenseLink = "https://opensource.org/licenses/MIT";
+      break;
+
+    case "Apache":
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0";
+      break;
+
+    case "GPL":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+      break;
+
+    case "BSD3":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+
+    default:
+      licenseLink = "This app has no license.";
+  }
+
+  //The beginning of the markdown document creation
+  return `
+[![Generic badge](https://img.shields.io/badge/license-${data.license}-<COLOR>.svg)](#license)
+# ${data.title}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Contributing](#contributing)
+* [License](#license)
+* [Questions](#questions)
+## Installation
+${data.install}
+## Tests
+${data.tests}
+## Usage
+${data.usage}
+## Contributing
+${data.contribute}
+## License
+### This app is licensed under the [${data.license}](${licenseLink}) license.
+## Questions
+* GitHub: [${data.github}](https://github.com/${data.github})
+* Email: 
+[${data.email}](mailto:${data.email})
+  `;
+};
+
+module.exports = generateMarkdown;
