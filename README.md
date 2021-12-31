5e450362242174c0894c24777d789ccc4093d7b0
scorpion77777 <vvmartirosyan@gmail.com>
2021-12-31 09:39:15 -0800

    Innitial commit

--- README.md
+++ README.md
@@ -1,29 +1,54 @@
+# Vahan
 
-[![Generic badge](https://img.shields.io/badge/license-MIT-<COLOR>.svg)](#license)
-# vahan
-## Description
-hhhh
-## Table of Contents
-* [Installation](#installation)
-* [Usage](#usage)
-* [Tests](#tests)
-* [Contributing](#contributing)
-* [License](#license)
-* [Questions](#questions)
-## Installation
-hhhh
-## Tests
-hhhh
-## Usage
-hhhvvt
-## Contributing
-hhhh
-## License
-### This app is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
-## Questions
-* GitHub: [hhhhh](https://github.com/hhhhh)
-* Link to video: hhhh
-* Link to screenshot: ![demo](/App/utils/images/screenshot.png)
-* Email: 
-[hhhh](mailto:hhhh)
-  
+![License](https://img.shields.io/badge/License-MIT-orange.svg) <br> [MIT](https://opensource.org/licenses/MIT)
+
+1.[ Description. ](#desc)
+<br>
+2.[ Installation. ](#inst)
+<br>
+3.[ Usage. ](#use)
+<br>
+4.[ Contributing. ](#contr)
+<br>
+5.[ Testing. ](#test)
+<br>
+6.[ Github. ](#git)
+<br>
+7.[ Contact Me.](#conta)
+<br>
+
+<a id="desc"></a>
+## 1.Description
+
+For generting good readme.md
+
+<a id="inst"></a>
+## 2.Installation
+
+npm init -y then npm i inquirer and finally node index.js
+
+<a id="use"></a>
+## 3.Usage
+
+Not available.
+
+<a id="contr"></a>
+## 4.Contributing
+
+Open source
+
+<a id="test"></a>
+## 5.Testing
+```
+Not available.
+```
+<a id="git"></a>
+## 6.Github
+
+[GitHub](https://github.com/scorpion77777)
+
+<a id="conta"></a>
+## 7.Contact Me
+Vahan
+vvmartirosyan@gmail.com
+
--- index.js
+++ index.js
@@ -1,45 +1,147 @@
-//Packages and modules needed for this application
-const fs = require("fs");
-//obj destructure prompt method
-const { prompt } = require("inquirer");
-const questions = require("./questions.js");
-const init = async () => {
-  const { username, likeDog, likeParty, goals, imageURL } = await prompt(
-    questions
-  );
-  const template = `
-    <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <title>${username}'s Dating Profile</title>
-  </head>
-  <body>
-    <h1>HELLO MY NAME IS ${username}</h1>
-    <div class="card" style="width: 18rem">
-      <img src="${imageURL}" class="card-img-top" alt="dating profile img" />
-      <div class="card-body">
-        <h5 class="card-title">${username}</h5>
-        <p class="card-text">
-          
-        </p>
-        <a href="#" class="btn btn-primary">Submit</a>
-      </div>
-    </div>
-  </body>
-</html>
-    `;
+const inquirer = require('inquirer');
+const fs = require('fs');
+// ['MIT','GPLv2','Apache','Unlicense','BSD_3','BSD_4','unavailable']
 
-  fs.writeFileSync("./profile.html", template);
-  console.info("SUCCESS!");
-};
+const renderLicense = (license) => {
+    switch (license) {
+        case 'MIT':
+            return `![License](https://img.shields.io/badge/License-MIT-orange.svg) <br> [MIT](https://opensource.org/licenses/MIT)`;
+        case 'GPLv2':
+            return `[License](https://img.shields.io/badge/License-GPLv2-orange.svg) <br> [GPLv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+        case 'Apache':
+            return `[License](https://img.shields.io/badge/License-Apache-orange.svg) <br> [Apache](https://www.apache.org/licenses/LICENSE-2.0)]`;
+        case 'Unlicense':
+            return `[License](https://img.shields.io/badge/License-Unlicense-orange.svg) <br> [unlicense](https://unlicense.org/)`;
+        case 'BSD_3':
+            return `[License](https://img.shields.io/badge/License-BSD_3-orange.svg) <br> [BSD_3](https://opensource.org/licenses/BSD-3-Clause)`;
+        case 'BSD_4':
+            return `[License](https://img.shields.io/badge/License-BSD_4-orange.svg) <br> [BSD_4](https://spdx.org/licenses/BSD-4-Clause.html)`;
+        case 'unavailable':
+            return `No license available for this project.`;
 
+    }
+}
+
+const generateReadme = ({ name, title, description, installation, usage, contribution, test, license, github, email }) =>
+    `# ${title}
+
+${renderLicense(license)}
+
+1.[ Description. ](#desc)
+<br>
+2.[ Installation. ](#inst)
+<br>
+3.[ Usage. ](#use)
+<br>
+4.[ Contributing. ](#contr)
+<br>
+5.[ Testing. ](#test)
+<br>
+6.[ Github. ](#git)
+<br>
+7.[ Contact Me.](#conta)
+<br>
+
+<a id="desc"></a>
+## 1.Description
+
+${description}
+
+<a id="inst"></a>
+## 2.Installation
+
+${installation}
+
+<a id="use"></a>
+## 3.Usage
+
+${usage}
+
+<a id="contr"></a>
+## 4.Contributing
+
+${contribution}
+
+<a id="test"></a>
+## 5.Testing
+\`\`\`
+${test}
+\`\`\`
+<a id="git"></a>
+## 6.Github
+
+[GitHub](https://github.com/${github})
+
+<a id="conta"></a>
+## 7.Contact Me
+${name}
+${email}
+
+`
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name if you would like to add it to your README.',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of your application.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please enter instructions on how to install your application.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please enter an explanation the usage information for your application.',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please enter your contribution guidelines for people who may want to contribute to your application.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please enter any test instructions you would like to give.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose the license you used for your application.',
+            choices: ['MIT', 'GPLv2', 'Apache', 'Unlicense', 'BSD_3', 'BSD_4', 'unavailable']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your github username. (will be added to a link to your repository)'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address where users can contact you with anyone questions.'
+        },
+
+
+
+    ])
+}
+
+const init = () => {
+    promptUser()
+
+        .then((answers) => fs.writeFileSync('README.md', generateReadme(answers)))
+        .then(() => console.log('Successfully created your README.md!'))
+        .catch((err) => console.log(err));
+}
 init();
